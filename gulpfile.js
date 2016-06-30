b391605f76928f765aec7b6259c46227dfb7b175
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2016-06-30 20:55:34 +0200

    incluyendo nuevo archvios de configuracion gulpfile package

--- gulpfile.js
+++ gulpfile.js
@@ -1,122 +1,152 @@
 var gulp = require('gulp');
 postcss = require('gulp-postcss');
-nested = require('postcss-nested');
-pxtorem = require('postcss-pxtorem');
+autoprefixer = require('gulp-autoprefixer');
 sourcemaps = require('gulp-sourcemaps');
-postCSS_InlineComment = require('postcss-inline-comment');
 atImport = require('postcss-import');
 cssnext = require('postcss-cssnext');
 sorting = require('postcss-sorting');
-autoprefixer = require('gulp-autoprefixer');
-imagemin = require('gulp-imagemin');
-pngquant = require('imagemin-pngquant');
+nested = require('postcss-nested');
+pxtorem = require('postcss-pxtorem');
+reporter = require('postcss-reporter');
 uglify = require('gulp-uglify');
 newer = require('gulp-newer');
-rename = require('gulp-rename');
 nano = require('gulp-cssnano');
 notify = require("gulp-notify");
+stylelint = require('stylelint');
+browserSync = require("browser-sync");
 
+gulp.task("browserSync", function() {
+  browserSync({
+    server: {
+      baseDir: "./"
+    }
+  })
+});
+
+
+/* Variables */
 var imgSrc = './src/img/*';
 var imgDist = './img';
 var jsSrc = './src/js/*.js';
 var jsDist = './js';
 
+/* Notificando errores de JavaScript */
 function errorAlertJS(error) {
-    notify.onError({
-        title: "Gulp JavaScript",
-        subtitle: "Algo esta mal en tu JavaScript!",
-        sound: "Basso"
-    })(error);
-    console.log(error.toString());
-    this.emit("end");
+  notify.onError({
+    title: "Gulp JavaScript",
+    subtitle: "Algo esta mal en tu JavaScript!",
+    sound: "Basso"
+  })(error);
+  console.log(error.toString());
+  this.emit("end");
 };
 
+/* Notificando errores de CSS */
 function errorAlertPost(error) {
-    notify.onError({
-        title: "Gulp postCSS",
-        subtitle: "Algo esta mal en tu CSS!",
-        sound: "Basso"
-    })(error);
-    console.log(error.toString());
-    this.emit("end");
+  notify.onError({
+    title: "Gulp postCSS",
+    subtitle: "Algo esta mal en tu CSS!",
+    sound: "Basso"
+  })(error);
+  console.log(error.toString());
+  this.emit("end");
 };
 
-gulp.task('imagemin', function() {
-    return gulp.src(imgSrc)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{
-                removeViewBox: false
-            }],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest(imgDist));
+/* Comprimiendo JavaScript */
+gulp.task('compress', function() {
+  return gulp.src(jsSrc)
+    .pipe(uglify())
+    .on("error", errorAlertJS)
+    .pipe(gulp.dest(jsDist))
+    .pipe(notify({
+      message: 'JavaScript complete'
+    }));
 });
 
+/* ==========================================================================
+   Lanzando postCSS
+   ========================================================================== */
 
-
-gulp.task('images', function() {
-    return gulp.src(imgSrc)
-        .pipe(newer(imgDist))
-        .pipe(imagemin())
-        .pipe(gulp.dest(imgDist));
-});
-
-gulp.task('js-uglify', function() {
-    return gulp.src(jsSrc)
-        .pipe(uglify())
-        .on("error", errorAlertJS)
-        .pipe(gulp.dest(jsDist))
-        .pipe(notify({
-            message: 'JavaScript complete'
-        }));
-});
+/*
+ * El orden de los plugins debe ser respetado.
+ *
+ * Antes de que nuestro CSS empiece a ser transformado por los diferentes
+ * plugins vamos a 'lintear' nuestro CSS para seguir un orden y concierto.
+ * este párrafo.
+ *
+ */
 
 gulp.task('css', function() {
-    var processors = [
-        atImport,
-        cssnext,
-        nested,
-        autoprefixer({
-            browsers: ['last 2 version']
-        }),
-        pxtorem({
-            root_value: 16,
-            unit_precision: 2,
-            prop_white_list: ['font', 'font-size', 'line-height', 'letter-spacing', 'margin', 'padding'],
-            replace: true,
-            media_query: false
-        }),
-        sorting({
-            "sort-order": "csscomb"
-        }),
-        postCSS_InlineComment
-    ];
-    return gulp.src('./src/css/styles.css')
-        .pipe(sourcemaps.init())
-        .pipe(postcss(processors))
-        .on("error", errorAlertPost)
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./css'))
-        .pipe(notify({
-            message: 'postCSS complete'
-        }));
+  var processors = [
+    stylelint(),
+    reporter({
+      clearMessages: true
+    }),
+    atImport,
+    nested,
+    cssnext,
+    pxtorem({
+      root_value: 16,
+      unit_precision: 2,
+      prop_white_list: ['font', 'font-size', 'line-height', 'letter-spacing', 'margin', 'padding'],
+      replace: true,
+      media_query: false
+    }),
+    sorting({
+      "sort-order": "csscomb"
+    }),
+    autoprefixer
+  ];
+  return gulp.src('./src/css/styles.css')
+
+  .pipe(sourcemaps.init())
+    .pipe(postcss(processors))
+    .on("error", errorAlertPost)
+    .pipe(sourcemaps.write('./css', {
+      sourceRoot: '/src'
+    }))
+    .pipe(gulp.dest('./css'))
+    .pipe(notify({
+      message: 'postCSS complete'
+    }));
 });
 
+/* Lanzando CSSnano para comprimir CSS */
 gulp.task('minify', function() {
-    return gulp.src('./css/styles.css')
-        .pipe(nano())
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./css'))
-        .pipe(notify({
-            message: 'CSSnano task complete'
-        }));
+  return gulp.src('./css/styles.css')
+    .pipe(nano())
+    .pipe(gulp.dest('./css'))
+    .pipe(notify({
+      message: 'CSSnano task complete'
+    }));
 });
 
-gulp.task('default', function() {
-    gulp.watch('./src/css/*.css', ['css']);
-    gulp.watch('./src/img/**', ['images']);
-    gulp.watch('./css/*.css', ['minify']);
+/* Comprimiendo imagenes */
+gulp.task('imagemin', function() {
+  return gulp.src(imgSrc)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(imgDist));
 });
+
+gulp.task('images', function() {
+  return gulp.src(imgSrc)
+    .pipe(newer(imgDist))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDist));
+});
+
+/* Tarea por defecto para compilar CSS y comprimir imagenes */
+gulp.task('default', ["browserSync"], function() {
+  gulp.watch('./src/css/*.css', ['css']);
+  gulp.watch('./src/img/**', ['images']);
+  gulp.watch(["./*.html", "css/*.css"]).on("change", browserSync.reload);
+});
+
+/* Tarea final para comprimir CSS y JavaScript */
+gulp.task('build', ['minify', 'compress']);
